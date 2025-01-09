2af7453c41fbe470bfb3b45be9ab63a1d5cf1a9b
TiagoMoraes <tiago.moraessilva@hotmail.com>
2025-01-09 04:26:43 -0300

    Implementação de melhorias na lógica de filtragem dos diversos tipos de camadas vetoriais

--- __init__.py
+++ __init__.py
@@ -5,5 +5,5 @@ Esse arquivo é necessário para que o QGIS reconheça a pasta como um plugin.
 """
 
 def classFactory(iface):
-    from .Filtra_Selecionados_V2 import FiltraSelecionadosPlugin
+    from .Filtra_Selecionados_V1_2 import FiltraSelecionadosPlugin
     return FiltraSelecionadosPlugin(iface)
